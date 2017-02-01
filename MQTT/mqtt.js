8fd0930f0102044c439fcab23c6078fcfd0cb22b
kaushal1214 <bonjourandoird.hotmail.com>
2017-02-01 11:39:33 +0530

    Sensor Code Uploaded

--- MQTT/mqtt.js
+++ MQTT/mqtt.js
@@ -1,17 +1,14 @@
 var mqtt = require('mqtt'),
-	client = mqtt.connect('mqtt://127.0.0.1');
+	client = mqtt.connect('mqtt://192.168.43.201');
 var JSON = require('JSON');
 var http = require('http'),
 	querystring = require('querystring');
 
-//Dummy Data
-var DHT11 ="{Temp:23 ,Humidity:160, Device:'1m4uri7'}";
 
 //Connect callback for MQTT
 client.on('connect', function(){
 	console.log('MQTT Broker connected');
 	client.subscribe('hello');
-	client.publish('hello',DHT11);
 });
 
 //Subscribe
@@ -19,21 +16,17 @@ client.on('message', function(topic, msg){
 	var json = JSON.stringify(eval('('+msg.toString()+')'));
 	json = JSON.parse(json);
 	var payload={};
-	if(json.Temp)
+	
+	if(json.Data)
 	{
-		console.log('Temp:'+json.Temp);
-		payload.field2 = json.Temp;
+		console.log('Data:'+json.Data);
+		payload.data = json.Data;
 	}
-	if(json.Humidity)
-	{
-		console.log('Humidity:'+json.Humidity);
-		payload.data = json.Humidity;
-	}
-//	payload.api_key='VJUDF7AMIKTNC1TU';
+	
 	var postData = querystring.stringify(payload);	
 	
 	var options ={
-		hostname: 'localhost',
+		hostname: '192.168.43.201',
 		port: 3300,
 		path: '/devices/'+json.Device,
 		method: 'POST',
